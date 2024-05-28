b41098dd4f2a4019ac1db8a3a1280e299199ea1e
Madian2112 <madian.reyes212006@gmail.com>
2024-05-27 21:53:38 -0600

    feat(app): se hicieron cambios pequenios

--- app/presentaciones3/src/pages/departamento.tsx
+++ app/presentaciones3/src/pages/departamento.tsx
@@ -10,7 +10,7 @@ import {
 import { Field, Form, Formik } from 'formik'
 import { mdiAccount, mdiBallotOutline, mdiMail, mdiUpload } from '@mdi/js'
 import Head from 'next/head'
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { Dialog } from 'primereact/dialog'
 import type { ReactElement } from 'react'
 import Button from '../components/Button'
@@ -29,27 +29,59 @@ import ChartLineSample from '../components/ChartLineSample'
 import NotificationBar from '../components/NotificationBar'
 import TableSampleClients from '../components/Table/SampleClients'
 import { getPageTitle } from '../config'
-
+import { Toast } from 'primereact/toast';
 import { mdiEye, mdiTrashCan } from '@mdi/js'
 import CardBoxModal from '../components/CardBox/Modal'
 import FormField from '../components/Form/Field'
 import Divider from '../components/Divider'
 import Buttons from '../components/Buttons'
+
+import { ProductViewModel } from '../interfaces/telefonoViewModel'
 const DepartamentoPage = () => {
   const [visible, setVisible] = useState(false)
   const [isModalInfoActive, setIsModalInfoActive] = useState(false);
-
+  const toast = useRef<Toast>(null);
   const handleModalAction = () => {
     setIsModalInfoActive(false);
   }
 
-  const Mostrar = (values) => {
-    alert(JSON.stringify(values, null, 2));
+  const Mostrar = async (values) => {
+    const productData : ProductViewModel ={
+      name: values.name,
+      data: {
+        year: parseInt(values.year),
+        price: parseFloat(values.price),
+        "CPU model": values.CPU_model,
+        "Hard disk size": values.Hard_disk_size,
+      },
+    };
+
+
+    const response = await fetch('https://api.restful-api.dev/objects', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(productData),
+    });
+
+    if (response.ok) {
+      const responseData = await response.json();
+      console.log('Success:', responseData);
+      setIsModalInfoActive(false);
+      toast.current?.show({ severity: 'success', summary: 'Success', detail: `Product added successfully. Status Code: ${response.status}`, life: 100000 });
+    } else {
+      console.error('Error:', response.statusText);
+      alert('Failed to add product');
+      toast.current?.show({ severity: 'error', summary: 'Error', detail: `Failed to add product. Status Code: ${response.status}`, life: 3000 });
+    }
+
+    setIsModalInfoActive(false);
   }
   return (
     <>
-      <Button label="Open Modal" onClick={() => setIsModalInfoActive(true)} />
 
+<Toast ref={toast} />
 <CardBoxModal
   title="Agregar"
   buttonColor="info"
@@ -70,27 +102,25 @@ const DepartamentoPage = () => {
       Mostrar(values);
     }}
   >
-
+    {() => (
       <Form>
         <FormField>
           <Field name="name" placeholder="Nombre" />
-          <Field type="date" name="year" placeholder="Año" />
-        </FormField>
-
-        <FormField>
-          <Field name="price" placeholder="Precio" />
-          <Field type="text" name="CPU_model" placeholder="CPU modelo" />
+          <Field type="number" name="year" placeholder="Año" />
         </FormField>
         <FormField>
-          <Field name="price" placeholder="Precio" />
-          <Field type="text" name="Hard_disk_size" placeholder="Tamaño del disco" />
+          <Field type="number" name="price" placeholder="Precio" />
+          <Field name="CPU_model" placeholder="CPU modelo" />
+        </FormField>
+        <FormField>
+          <Field name="Hard_disk_size" placeholder="Tamaño del disco" />
         </FormField>
         <Buttons>
           <Button type="submit" label="Agregar" color="info" />
           <Button label="Cancelar" color="info" outline onClick={handleModalAction} />
         </Buttons>
       </Form>
-
+    )}
   </Formik>
 </CardBoxModal>
 
